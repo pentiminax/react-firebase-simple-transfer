e2733e2504a4ed18795256c52069b42951f169b8
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2022-11-09 14:57:15 +0100

    Change form file input ref to fileId state

--- src/pages/RequestFile/index.tsx
+++ src/pages/RequestFile/index.tsx
@@ -1,19 +1,23 @@
-import { FormEvent, useState } from "react";
+import { ChangeEvent, ChangeEventHandler, FormEvent, useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import firebaseService from "../../services/firebase";
 
 export default function RequestFile() {
-    const [inputFileRef, setInputFileRef] = useState<HTMLInputElement>();
+    const [fileId, setFileId] = useState<string>("");
     const navigate = useNavigate();
+
+    const handleFileIdChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFileId(e.target.value);
+    }
 
     const handleSubmit = async(e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const file = await firebaseService.getSingleFile(inputFileRef.value);
+        const file = await firebaseService.getSingleFile(fileId);
 
-        inputFileRef.value = "";
+        setFileId("");
 
         if (undefined !== file) {
             navigate(`/files/${file.id}`);
@@ -32,7 +36,18 @@ export default function RequestFile() {
             <Card.Header>Rechercher un fichier</Card.Header>
             <Card.Body>
                 <Form onSubmit={handleSubmit}>
-                    <Form.Control type="text" className="mb-3" name="fileId" ref={setInputFileRef} minLength={8} maxLength={8} max="8" placeholder="Exemple : YOM2W71F" required />
+                    
+                    <Form.Control 
+                    className="mb-3" 
+                    type="text" 
+                    value={fileId} 
+                    onChange={handleFileIdChange}
+                    minLength={8} 
+                    maxLength={8} 
+                    max="8" 
+                    placeholder="Exemple : YOM2W71F" 
+                    required />
+
                     <Button className="w-100" type="submit">Rechercher</Button>
                 </Form>
             </Card.Body>
